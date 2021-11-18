baf837091f0d097288b23651447fd7899f4c5561
eralieva <92498351+eralieva@users.noreply.github.com>
2021-11-18 13:34:39 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head >
+<body background="image/1.jpeg">
+    <h1>My name is nazelia.
+       <p>l am 18 years.</p> 
+ <p>l study at Ala-too University.</p>
+ <p> My profession is computer systems.</p>
+ <p>My contact :0500211003</p>
+ 
+ 
+    <img src="image/1.jpeg" alt="">
+</body>
+</html>
+
+</head >
+<body
+   href="https://web.whatsapp.com/">WhatsApp</a>
+ <a href="https://www.instagram.com/nazelayyy/">Instagram</a>
+ <a href="https://web.telegram.org/z/">Telegram</a>
+ </body>
+ </html>
+
+
+
+
+<li> <a href= li>
+    
